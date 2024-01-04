117e18135155f232cdfbc5ef052925cd6f6d44b7
Anand8216 <alizadixit6@gmail.com>
2024-01-04 13:01:14 +0530

    little change

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -77,7 +77,7 @@ const registerUser= asyncHandler(async (req,res)=>{
 
 
   return res.status(201).json(
-    new ApiResponse(200,createdUser,"user Regitered sucdesfully")
+    new ApiResponse(200,createdUser,"user Regitered succesfully")
   )
       
 })
